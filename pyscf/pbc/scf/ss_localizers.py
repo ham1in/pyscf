6d117210bb1adbc688a55e0fb8614c49eb909a75
stephen-quiton <stephen.quiton@berkeley.edu>
2024-05-01 17:17:32 -0700

    debugging pt 2

--- pyscf/pbc/scf/ss_localizers.py
+++ pyscf/pbc/scf/ss_localizers.py
@@ -42,7 +42,7 @@ def localizer_int_exp_2d(x, r1, d):
 
 
 
-    h = lambda x: quad(g, -1, x)[0] / int_g if (x != -1 and x != 1) else 0
+    h = lambda x: quad(g, -1, x,points=[-1,1])[0] / int_g if (x != -1) else 0
     f = lambda y: h(1 - 2 * np.abs(y))
     print(max(r),min(r))
     val = [f(ri) for ri in r]
